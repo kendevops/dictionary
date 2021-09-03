c54ecfe0811fe112e21845cea9faf5ff847ec8f9
Kenneth <kendevops@gmail.com>
2021-09-03 12:14:34 +0100

    Created the dictionary application structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [lang, setLang] = useState("");
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const dictApi = async (lang, word) => {
+    try {
+      const api = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${lang}/${word}`
+      );
+      setMeanings(api.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    dictApi("en", "hello");
+  }, []);
+
+  return <div className="App">{meanings}</div>;
+};
 
 export default App;
